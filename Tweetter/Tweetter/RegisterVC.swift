756ff15835a3b5e2cc7e4fbff3a58784c54f750c
Kruijff, Geert-Jan <gj.kruijff@gmail.com>
2017-02-14 22:17:12 +0100

    Register viewcontroller, set up properly with Stack views

---
+++ Tweetter/Tweetter/RegisterVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tweetter
+//
+//  Created by Geert-Jan Kruijff on 14/02/17.
+//  Copyright © 2017 Geert-Jan Kruijff. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
