c553f7f28d608697108103eb8dbdc358088eedf7
Kruijff, Geert-Jan <gj.kruijff@gmail.com>
2017-02-19 14:00:59 +0100

    User registration in Swift code, (not yet refactored)

--- Tweetter/Tweetter/RegisterVC.swift
+++ Tweetter/Tweetter/RegisterVC.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  Tweetter
@@ -40,13 +41,66 @@ class RegisterVC: UIViewController {
     // Trigger when the registration button gets clicked
     @IBAction func register_click(_ sender: Any) {
         // if any of the fields is empty, create a red placeholder
+        print("CLICK CLICK CLICK!!")
+        
+        
         if (PRES.inputFieldsEmpty(fields : inputFields))
         {
             // Request the VIEW to color the empty fields
             VIEW.colorEmptyFields(fields: inputFields)
-        } else {
-            // Request the INTR for some application logical stuff
+            print("Sorry fields are considered empty")
             
+        } else {
+        
+        
+            // Set up the request
+            // To get a secure connection, we need to edit info.plist
+            // Add App Transport Security Settings, (+) Allow Arbitrary Loads > "Yes" 
+            let url = URL(string: "http://localhost/Tweetter/TweetterBackend/register.php")!
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            print("Request: \(request)")
+            // Body for the request
+            let body = "username=\(usernameTxt.text!)&password=\(passwordTxt.text!)&email=\(emailTxt.text!)&fullname=\(firstnameTxt.text!)%20\(lastnameTxt.text!)"
+            request.httpBody = body.data(using: String.Encoding.utf8)
+            
+            print("Request: \(request)")
+            print("RequestBody: \(request.httpBody)")
+            //
+            URLSession.shared.dataTask(with: request as URLRequest, completionHandler: { (data:Data?, response: URLResponse?, error:Error?) in
+                
+                    if (error == nil) {
+                        // send the request to the server
+                        print("Going to send the request to the server")
+                        // communicate back to UI
+                        DispatchQueue.main.async(execute: {
+                            do {
+                                // let json = try JSONSerialization.JSONObjectWithData(data!,options: []) as NSDictionary
+                                // get the JSON result
+                                print(NSString(data: data!, encoding: String.Encoding.utf8.rawValue))
+                                let json  = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+                                // assign the JSON result
+                                // if..then combination with try/catch
+                                guard let parseJSON = json else {
+                                    print ("Error while parsing JSON")
+                                    return
+                                }
+                                // Get the ID from the JSON struct
+                                let id = parseJSON["id"]
+                                // if there is an ID then print the JSON
+                                if (id != nil) {
+                                    print(parseJSON)
+                                } // end if
+                            } catch {
+                                print("Caught an error: \(error)")
+                            }
+                           
+                        } )
+                    } else {
+                        print("Error in sending request: \(error)")
+                    }
+            // launch prepared session
+            }).resume()
             
             
             
