b2b305b09e1089c726f02fd51f3bccb1acc494e1
Kruijff, Geert-Jan <gj.kruijff@gmail.com>
2017-02-19 14:12:48 +0100

    Refactored user registration to Interactor

--- Tweetter/Tweetter/Interactor.swift
+++ Tweetter/Tweetter/Interactor.swift
@@ -10,7 +10,49 @@ import Foundation
 
 class Interactor {
 
-
-
+    func registerUser (username: String, password: String, email: String, firstname: String, lastname: String) {
+            // Set up the request
+            // To get a secure connection, we need to edit info.plist
+            // Add App Transport Security Settings, (+) Allow Arbitrary Loads > "Yes" 
+            let url = URL(string: "http://localhost/Tweetter/TweetterBackend/register.php")!
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            // Body for the request
+            let body = "username=\(username)&password=\(password)&email=\(email)&fullname=\(firstname)%20\(lastname)"
+            request.httpBody = body.data(using: String.Encoding.utf8)
+            // Set up the URL session to post the user profile data, and get the response
+            URLSession.shared.dataTask(with: request as URLRequest, completionHandler: { (data:Data?, response: URLResponse?, error:Error?) in
+                
+                    if (error == nil) {
+                        // dispatch the request to the server and get the response
+                        DispatchQueue.main.async(execute: {
+                            do {
+                                // let json = try JSONSerialization.JSONObjectWithData(data!,options: []) as NSDictionary
+                                // get the JSON result
+                                // print(NSString(data: data!, encoding: String.Encoding.utf8.rawValue))
+                                let json  = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+                                // assign the JSON result
+                                // if..then combination with try/catch
+                                guard let parseJSON = json else {
+                                    print ("Error while parsing JSON")
+                                    return
+                                }
+                                // Get the ID from the JSON struct
+                                let id = parseJSON["id"]
+                                // if there is an ID then print the JSON
+                                if (id != nil) {
+                                    print(parseJSON)
+                                } // end if
+                            } catch {
+                                print("Caught an error: \(error)")
+                            }
+                           
+                        } )
+                    } else {
+                        print("Error in sending request: \(error)")
+                    }
+            // launch prepared session
+            }).resume()
+    } // end func 
 
 } // end class
